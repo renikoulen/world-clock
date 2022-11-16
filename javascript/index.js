9f6ff4672376bb422a0d288d3eb5f9c492d58da9
renikoulen <92055660+renikoulen@users.noreply.github.com>
2022-11-16 21:40:21 +0100

    Added JS to the project
    Removed static information and implemented dynamic date and time information based on time and timezone.
    SetInterval to update the current time every 1 second

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,34 @@
+setInterval(function () {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#Los-Angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do, YYYY");
+  losAngelesTimeElement.innerHTML = moment
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
 
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyDateElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("MMMM Do, YYYY");
+  sydneyTimeElement.innerHTML = moment
+    .tz("Australia/Sydney")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  // Berlin
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  berlinDateElement.innerHTML = moment()
+    .tz("Europe/Berlin")
+    .format("MMMM Do, YYYY");
+  berlinTimeElement.innerHTML = moment
+    .tz("Europe/Berlin")
+    .format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
