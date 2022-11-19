2294c6c89eee528bbcc5f913871279b7275e8c58
renikoulen <92055660+renikoulen@users.noreply.github.com>
2022-11-19 14:49:44 +0100

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -54,6 +54,7 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
         </div>
+        <a href="index.html">All cities</a>
  `;
 }
 
