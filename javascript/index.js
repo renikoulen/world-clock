29fd372b649669dae7f8ec6f833ac2aac10494ee
renikoulen <92055660+renikoulen@users.noreply.github.com>
2022-11-19 14:25:39 +0100

    Added functionality to the select
    Picking a city from the drop-down and show the current time there.

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,61 @@
-setInterval(function () {
+function updateTime() {
   //Los Angeles
   let losAngelesElement = document.querySelector("#Los-Angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  losAngelesDateElement.innerHTML = moment()
-    .tz("America/Los_Angeles")
-    .format("MMMM Do, YYYY");
-  losAngelesTimeElement.innerHTML = moment
-    .tz("America/Los_Angeles")
-    .format("h:mm:ss [<small>]A[</small>]");
-
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    losAngelesDateElement.innerHTML = moment()
+      .tz("America/Los_Angeles")
+      .format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = moment
+      .tz("America/Los_Angeles")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
   // Sydney
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  sydneyDateElement.innerHTML = moment()
-    .tz("Australia/Sydney")
-    .format("MMMM Do, YYYY");
-  sydneyTimeElement.innerHTML = moment
-    .tz("Australia/Sydney")
-    .format("h:mm:ss [<small>]A[</small>]");
-
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    sydneyDateElement.innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = moment
+      .tz("Australia/Sydney")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
   // Berlin
   let berlinElement = document.querySelector("#berlin");
-  let berlinDateElement = berlinElement.querySelector(".date");
-  let berlinTimeElement = berlinElement.querySelector(".time");
-  berlinDateElement.innerHTML = moment()
-    .tz("Europe/Berlin")
-    .format("MMMM Do, YYYY");
-  berlinTimeElement.innerHTML = moment
-    .tz("Europe/Berlin")
-    .format("h:mm:ss [<small>]A[</small>]");
-}, 1000);
+  if (berlinElement) {
+    let berlinDateElement = berlinElement.querySelector(".date");
+    let berlinTimeElement = berlinElement.querySelector(".time");
+    berlinDateElement.innerHTML = moment()
+      .tz("Europe/Berlin")
+      .format("MMMM Do, YYYY");
+    berlinTimeElement.innerHTML = moment
+      .tz("Europe/Berlin")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
+}
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+ <div class="city">
+ <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>
+ `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
